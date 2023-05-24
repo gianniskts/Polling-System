04c88359cdea736cfa4bca51230cdb01421b729e
gianniskts <johnkotsias@hotmail.gr>
2023-05-24 18:06:36 +0300

    k

--- demo2.cpp
+++ demo2.cpp
@@ -10,13 +10,16 @@
 
 using namespace std;
 
+// Function to send a vote to the server
 void sendVote(const string &serverName, int portNum, const string &name_vote) {
+    // Create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("Error creating socket");
         return;
     }
 
+    // Specify server address
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portNum);
@@ -26,16 +29,19 @@ void sendVote(const string &serverName, int portNum, const string &name_vote) {
         return;
     }
 
+    // Connect to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error connecting to server");
         close(sock);
         return;
     }
 
+    // Send vote to the server
     if (send(sock, name_vote.c_str(), name_vote.size(), 0) < 0) {
         perror("Error sending vote");
     }
 
+    // Read confirmation from server
     char buffer[256];
     memset(buffer, 0, sizeof(buffer));
     if (read(sock, buffer, sizeof(buffer) - 1) < 0) {
@@ -44,30 +50,38 @@ void sendVote(const string &serverName, int portNum, const string &name_vote) {
         cout << "Server confirmation: " << buffer << "\n";
     }
 
+    // Close the socket
     close(sock);
 }
 
 int main(int argc, char *argv[]) {
+    // Check command line arguments
     if (argc != 4) {
         cerr << "Usage: " << argv[0] << " [serverName] [portNum] [inputFile.txt]\n";
         exit(1);
     }
 
+    // Parse command line arguments
     string serverName = argv[1];
     int portNum = atoi(argv[2]);
     string inputFile = argv[3];
 
+    // Open input file
     ifstream inFile(inputFile);
     if (!inFile) {
         cerr << "Unable to open input file: " << inputFile << "\n";
         exit(1);
     }
 
+    // For each line in the input file, create a new thread to send the vote
     string line;
     vector<thread> workers;
     while (getline(inFile, line)) {
         workers.push_back(thread(sendVote, serverName, portNum, line));
+
         // Wait for current thread to complete before moving to the next one.
+        // Note: This effectively makes the client single-threaded because it
+        // waits for each thread to finish before starting the next one.
         workers.back().join();
     }
 
