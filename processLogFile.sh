4fcf9a9415bc13190a4640359b716d207f5de85c
gianniskts <johnkotsias@hotmail.gr>
2023-05-25 01:26:10 +0300

    cv

---
+++ processLogFile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the number of arguments is correct
+if [ $# -ne 1 ]; then
+    echo "Usage: ./processLogFile.sh poll-log"
+    exit 1
+fi
+
+# Variables
+poll_log=$1
+poller_results_file="pollerResultsFile.txt"
+
+# Check if poll-log exists and has read permissions
+if [ ! -f $poll_log ] || [ ! -r $poll_log ]; then
+    echo "Poll log file does not exist or doesn't have read permissions."
+    exit 1
+fi
+
+# Reading poll-log and tally the votes
+awk 'BEGIN {OFS=FS=" "; total = 0} !seen[$1]++ {votes[$2]++; total++} END {for (i in votes) print i, votes[i]; print "TOTAL", total}' $poll_log > $poller_results_file
