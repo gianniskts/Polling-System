9ff1ba765a269268a36b85d79221bd40746db517
gianniskts <johnkotsias@hotmail.gr>
2023-05-29 15:58:48 +0300

    d

--- scripts/create_input.sh
+++ scripts/create_input.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check number of input arguments
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 2 ]; then # checks if the number of arguments is different from 2
     echo "Usage: ./create_input.sh politicalParties.txt numLines"
     exit 1
 fi
 
 # Assigning arguments to variables
-parties_file="../data/$1"
-num_lines=$2
+parties_file="../data/$1" # political parties file which is located in the data folder
+num_lines=$2 # number of lines to be generated
 
 # Check if the political parties file exists
 if [ ! -f $parties_file ]; then
@@ -19,7 +19,8 @@ fi
 # Generating random names, surnames and parties
 for (( i=0; i<$num_lines; i++ ))
 do
-    name=$(head /dev/urandom | tr -dc A-Za-z | head -c $((RANDOM%10+3)))
+    firstName=$(head /dev/urandom | tr -dc A-Za-z | head -c $((RANDOM%10+3)) | sed -e "s/\b\(.\)/\u\1/g") # random first name with length between 3 and 12
+    surname=$(head /dev/urandom | tr -dc A-Za-z | head -c $((RANDOM%10+3)) | sed -e "s/\b\(.\)/\u\1/g") # random surname with length between 3 and 12
     party=$(shuf -n 1 $parties_file)
-    echo "$name $party" >> "../data/inputFile.txt"
+    echo "$firstName $surname $party" >> "../data/inputFile.txt"
 done
